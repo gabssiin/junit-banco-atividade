a9483a1a5be5ff3f8c1d7636440033e4b6e56d6d
Gabriel Santos <163359098+gabssiin@users.noreply.github.com>
2025-08-30 15:02:59 +0000

    Atividade 1. 30/08

--- src/test/java/BancoTest.java
+++ src/test/java/BancoTest.java
@@ -1,6 +1,48 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import com.example.Banco;
 
 public class BancoTest {
 
-    
+    @BeforeEach
+    public void setUp(){
+        Banco bancoCompleto = new Banco("Banco Completo", 10000);
+    }
+    @Test
+    public void balanceNewAccountTest() {
+        Banco b = new Banco("777", 1000);
+        assertEquals(1000, b.getBalance());
+    }
+
+    @Test
+    public void depositNewAccountTest(){
+        Banco d = new Banco("dep", 1000);
+        d.deposit(500);
+        assertEquals(1500, d.getBalance());
+    }
+
+    @Test
+    public void depositInvalidTest(){
+        Banco d = new Banco("dep", 1000);
+        d.deposit(-500);
+        assertEquals(1000, d.getBalance());
+    }
+    @Test
+    public void withdrawNewAccountTest(){
+        Banco w = new Banco("withdraw", 1000);
+        w.withdraw(400);
+        assertEquals(600, w.getBalance());
+    }
+
+    @Test
+    public void balanceInvalidTest(){
+        Banco bIn = new Banco("inv", 1000);
+        bIn.withdraw(1500);
+        assertFalse(false, "False deu erro ai rapaziada");
+
+    }
 }
